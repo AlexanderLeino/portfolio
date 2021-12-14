eff7fcaf56c38b033ef2eb04be3735b9f7d316b6
Alexander Leino <Alexander.Leino@yahoo.com>
2021-12-14 15:59:50 -0500

    refactored css and made css files for each component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,28 @@
 import React from "react";
+import NavBar from './Navbar.css'
 
 const navBar = () => {
     return (
-<div className='container-fluid d-flex justify-content-center mt-2 hero'>
-  <div className= 'd-flex justify-content-center navBar '>
-    <div className= 'row no-gutters'>
-          <h2 className= 'name'>Alexander Leino</h2>
-        <div className='container btnContainer'>
+<div className='container justify-content-space-around mt-2 hero'>
+    <div className= 'row'>
+          <h2 className= 'col-12 name'>Alexander Leino</h2>
+    </div>  
+          <div className="row ">
+            <div className="col-lg-3 col-md-3 col-sm-12">
             <button className='navButtons'>Projects</button>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-12">
             <button className='navButtons'>About Me</button>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-12">
             <button className='navButtons'>Contact Me</button>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-12">
             <button className='navButtons'>Resume</button>
+            </div>
           </div>
-        </div>     
-    </div>
-  </div>
+          </div>
+          
 
 
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import SkillsCss from './Skills.css'
 
 const Skills = () => {
     return (
