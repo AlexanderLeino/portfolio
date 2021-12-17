a9736cf036c5e981d3f6c060e1f1b32d3fa64f12
Alexander Leino <Alexander.Leino@yahoo.com>
2021-12-16 19:12:46 -0500

    finishing touches fixed overflow issue with only plants card

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -7,7 +7,7 @@ import netflixAndGrillPic from '../../src/Assets/net-grill-screencap.png'
 
 const ProjectContainer = () => {
     return (
-    <div className= 'container-fluid flex-column justify-content-evenly mt-4'>
+<div className= 'container-fluid flex-column justify-content-evenly mt-4'>
         <div className="row">
             <h1 className= 'title' id="projects">Projects</h1>
             <h3 className='project-title'>Netflix and Grill</h3>
@@ -21,19 +21,19 @@ const ProjectContainer = () => {
             <div className="col-lg-6">
                 <div className= 'project-description'>Description</div>
                 <p className= 'project-description-paragraph'>The goal of the project was to solve two problems that every couple have on a regular basis. Those being what are we going to eat? What should we watch on TV tonight? With this app we take in a few responses from the end user and based on their responses to the three questions we generate a movie based on their prefernce when it comes to film genre. As well as a meal that meets their minimum servings based on how many people the user imputs.</p>
-                <div>Built With:<div>
+                <div className="built-with">Built With:<div>
                 </div>
     <div className="container-fluid d-flex justify-content-evenly">
             <div col-lg-2>
-                <FontAwesomeIcon className = 'built-with-icon mt-3' icon={['fab', 'html5']} />
+                <FontAwesomeIcon className = 'built-with-icon mt-3 horizontal-icons' icon={['fab', 'html5']} />
             </div>
 
             <div col-lg-2>
-                <FontAwesomeIcon className = 'built-with-icon mt-3' icon={['fab', 'js-square']} />
+                <FontAwesomeIcon className = 'built-with-icon mt-3 horizontal-icons' icon={['fab', 'js-square']} />
             </div>
 
             <div col-lg-2>
-                <FontAwesomeIcon className = 'built-with-icon mt-3' icon={['fab', 'css3-alt']} />
+                <FontAwesomeIcon className = 'built-with-icon mt-3 horizontal-icons' icon={['fab', 'css3-alt']} />
             </div>
 
             <div col-lg-2>
@@ -52,110 +52,111 @@ const ProjectContainer = () => {
            
                 
                
-            <h3 className='project-title'>Black Jack</h3>
-
-    <div className= 'projectCard container d-flex'>
-        <div className="row">
-            <div className="col-lg-6">
-                <img src = {blackJackPicture} alt=''></img>
-            </div>
-            
-            <div className="col-lg-6">
-                <div className= 'project-description'>Description</div>
-                <p className= 'project-description-paragraph'>The goal of the project was to focus on writing dense programming logic when I first started writing code as well as focus on heavy DOM manipulation by creating each individual card using BootStrap, CSS, vanilla javascript. </p>
-                <div>Built With:<div>
+    <h3 className='project-title'>Black Jack</h3>
+        <div className= 'projectCard container d-flex'>
+            <div className="row">
+                <div className="col-lg-6">
+                    <img src = {blackJackPicture} alt=''></img>
                 </div>
-    <div className="container-fluid d-flex justify-content-evenly">
-            <div>
-                <FontAwesomeIcon className = 'built-with-icon mt-3' icon={['fab', 'html5']} />
-            </div>
+                
+                <div className="col-lg-6">
+                    <div className= 'project-description'>Description</div>
+                    <p className= 'project-description-paragraph'>The goal of the project was to focus on writing dense programming logic when I first started writing code as well as focus on heavy DOM manipulation by creating each individual card using BootStrap, CSS, vanilla javascript. </p>
+                    <div className="built-with">Built With:<div>
+                </div>
+        <div className="container-fluid d-flex justify-content-evenly">
+                <div>
+                    <FontAwesomeIcon className = 'built-with-icon mt-3 horizontal-icons' icon={['fab', 'html5']} />
+                </div>
 
-            <div>
-                <FontAwesomeIcon className = 'built-with-icon mt-3' icon={['fab', 'js-square']} />
-            </div>
+                <div>
+                    <FontAwesomeIcon className = 'built-with-icon mt-3 horizontal-icons' icon={['fab', 'js-square']} />
+                </div>
 
-            <div >
-                <FontAwesomeIcon className = 'built-with-icon mt-3' icon={['fab', 'css3-alt']} />
-            </div>
+                <div >
+                    <FontAwesomeIcon className = 'built-with-icon mt-3 horizontal-icons' icon={['fab', 'css3-alt']} />
+                </div>
 
-            <div>
-                <h6 className="word-logo">Bootstrap</h6>
-            </div>
+                <div>
+                    <h6 className="word-logo">Bootstrap</h6>
+                </div>
 
+            </div>
         </div>
-    </div>
-            <div className="row mt-4">
-            <a target= '_blank' rel='noreferrer' projectBtn href= 'https://alexanderleino.github.io/blackjack-game/'><button className='projectBtn'>Deploy App</button></a>
-            <a target= '_blank' rel='noreferrer' projectBtn href= 'https://github.com/AlexanderLeino/blackjack-game'><button className='projectBtn'>View Source Code</button></a>
-            </div>
+                <div className="row mt-4">
+                <a target= '_blank' rel='noreferrer' projectBtn href= 'https://alexanderleino.github.io/blackjack-game/'><button className='projectBtn'>Deploy App</button></a>
+                <a target= '_blank' rel='noreferrer' projectBtn href= 'https://github.com/AlexanderLeino/blackjack-game'><button className='projectBtn'>View Source Code</button></a>
                 </div>
                     </div>
-                </div>
+                        </div>
+        </div>
 
-    
-                <h3 className='project-title'>Only Plants</h3>
-                <div className= 'projectCard container d-flex'>
-        <div className="row">
-            <div className="col-lg-6">
-                <img src = {onlyPlantsPicture} alt=''></img>
-            </div>
-            
-            <div className="col-lg-6">
-                <div className= 'project-description'>Description</div>
-                <p className= 'project-description-paragraph'>This was my bootcamps teams 2nd project where the focus was creating an app where individuals can create posts, create an account and sign in using the npm bycrpt to authinicate the user.</p>
-                <div>Built With:<div>
+
+
+
+        <h3 className='project-title'>Only Plants</h3>
+        <div className= 'projectCard container d-flex'>
+            <div className="row">
+                <div className="col-lg-6">
+                    <img src = {onlyPlantsPicture} alt=''></img>
                 </div>
-    <div className="container-fluid d-flex flex-column justifty-content-space-evenly">
-        
-    
-            <div className="container-fluid d-flex justify-content-evenly">
+                
+                <div className="col-lg-6">
+                    <div className= 'project-description'>Description</div>
+                    <p className= 'project-description-paragraph'>This was my bootcamps teams 2nd project where the focus was creating an app where individuals can create posts, create an account and sign in using the npm bycrpt to authinicate the user.  </p>
+                    <div className="built-with">Built With:<div>
+                </div>
+        <div className="container-fluid d-flex justify-content-evenly">
+            <div className="row">
                 <div>
                     <FontAwesomeIcon className = 'built-with-icon mt-3' icon={['fab', 'html5']} />
                 </div>
 
                 <div>
-                    <FontAwesomeIcon className = 'built-with-icon mt-3' icon={['fab', 'js-square']} />
+                    <FontAwesomeIcon className = 'built-with-icon mt-3 onlyplants-icon mt-5' icon={['fab', 'js-square']} />
                 </div>
 
-                <div>
-                    <FontAwesomeIcon className = 'built-with-icon mt-3' icon={['fab', 'css3-alt']} />
+                <div >
+                    <FontAwesomeIcon className = 'built-with-icon mt-3 onlyplants-icon mt-5' icon={['fab', 'css3-alt']} />
                 </div>
 
             </div>
-        
 
-        <div className="row mt-4">
-            <div className="container-fluid d-flex justify-content-evenly">
-            <div>
-                <h6 className= "word-logo">Bootstrap</h6>
-            </div>
+            <div className="row">
+               <div className="word-logo">
+                   BootStrap
+               </div>
 
-            <div>
-                <h6 className= "word-logo">MySQL</h6>
-            </div>
+               <div className="word-logo">
+                   Node
+               </div>
 
-            <div>
-                <h6 className= "word-logo">Express.js</h6>
-            </div>
+               <div className="word-logo">
+                   Express
+               </div>
 
             </div>
+    
+           
 
+           
+
+            </div>
         </div>
-        </div>
-    </div>
-            <div className="row mt-4">
-            <a target= '_blank' rel='noreferrer' projectBtn href= 'https://github.com/sarahmaskill/only-plants'><button className='projectBtn'>Deploy App</button></a>
-            <a target= '_blank' rel='noreferrer' projectBtn href= 'https://only-plants.herokuapp.com/'><button className='projectBtn'>View Source Code</button></a>
-            </div>
+                <div className="row mt-4">
+                <a target= '_blank' rel='noreferrer' projectBtn href= 'https://alexanderleino.github.io/blackjack-game/'><button className='projectBtn'>Deploy App</button></a>
+                <a target= '_blank' rel='noreferrer' projectBtn href= 'https://github.com/AlexanderLeino/blackjack-game'><button className='projectBtn'>View Source Code</button></a>
                 </div>
                     </div>
-                </div>
+                        </div>
+        </div>
+        
 
-
+    
 
 
            
-                </div>
+</div>
                
             
 
